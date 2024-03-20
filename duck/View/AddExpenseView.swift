fe1f15d5ffe0b33a9ee31e2650b337793df7a210
troyfung <fungtroy63@gmail.com>
2024-03-20 17:28:47 +0800

    refactor file

--- duck/View/AddExpenseView.swift
+++ duck/View/AddExpenseView.swift
@@ -1,73 +1,18 @@
+//
+//  AddExpenseView.swift
+//  duck
+//
+//  Created by troyfung on 20/3/2024.
+//
+
 import SwiftUI
-import SwiftData
+
 struct AddExpenseView: View {
-
-    @Environment(\.dismiss) private var dismiss
-    @Environment(\.modelContext) private var modelContext
-
-    @State private var title: String = ""
-    @State private var subtitle: String = ""
-
-    @State private var date: Date = .init()
-        @State private var amount: CGFloat = 0 
-    @State private var category: String = ""
-    @Query(animation: .snappy) private var allCategories: [Category]
-    var body: some View{
-        NavigationStack{
-            List{
-Section("Title"){
-    TextField("Magic Keyboard", text: $title)
-}
-Section("subtitle"){
-TextField("Magic Keyboard", text: $subtitle)
-}
-Section("Amount"){
-    Text("$").fontWeight(.semibold)
-    TextField("0.0", value: $amount, format: .currency(code: "HKD"))
-}
-Section("Date"){
-    DatePicker("", selection: $date, displayedComponents: [.date])
-    .datePickerStyle(.graphical)
-    .labelsHidden()
-}
-if !allCategories.isEmpty{
-HStack{
-    Text("Category")
-    Spacer()
-Picker("",selection: $category){
-    ForEach(allCategories){
-
-        Text($0.categoryName)
-        .tag($0)
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-}.pickerStyle(.menu)
-.labelsHidden()
-}}}
-            .navigationTitle("Add Expense")
-            .toolbar(
-                ToolbarItem(placement: .topBarLeading){
-                    Button ("Cancel"){
-                        dismiss()
-                    }
-                    .tint( .red)
-                },
+}
 
-                ToolbarItem(placement: .topBarTrailing){
-                    Button("Add", action: addExpense){
-                        saveExpense()
-                    }
-                }
-            )
-        }
-    }
-    func addExpense(){
-        let newExpense = Expense(context: modelContext)
-        newExpense.title = title
-        newExpense.subtitle = subtitle
-        newExpense.date = date
-        newExpense.amount = amount
-        newExpense.category = category
-        modelContext.save()
-        dismiss()
-    }
+#Preview {
+    AddExpenseView()
 }
